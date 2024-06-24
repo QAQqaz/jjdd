b51fe2a583d356720ad7f4a4901c1f56a10895c4
QAQqaz <108590692+QAQqaz@users.noreply.github.com>
2024-06-24 23:34:07 +0800

    Update jd_getcookie1.js

--- jd_getcookie1.js
+++ jd_getcookie1.js
@@ -1,17 +1,30 @@
 /**************************************
 [rewrite_local]
-^http[s]?:\/\/api\.m\.jd\.com\/client\.action\? url script-response-body https://raw.githubusercontent.com/QAQqaz/jjdd/main/jd_getcookie1.js
+^http[s]?:\/\/api\.m\.jd\.com\/client\.action\? url script-response-body https://raw.githubusercontent.com/QAQqaz/jjdd/main/jd_getcookie.js
 
 [mitm]
-hostname = api.m.jd.com
+hostname = mars.jd.com
 *****************************************/
 
+// 获取Cookie并打印pt_pin和pt_key
+function getCookiesAndLog() {
+    let headerCookie = $request.headers["Cookie"];
+    let pt_pin_match = headerCookie.match(/pt_pin=([^;]+)/);
+    let pt_key_match = headerCookie.match(/pt_key=([^;]+)/);
 
-let headerCookie = $request.headers["Cookie"];
-console.log("获取京东Cookie"+headerCookie);
-if(headerCookie){
-	$notify("更新京东Cookie成功🎉", "", "无需禁用脚本，仅cookie改变时才会重新获取");
-}else{
-	$notify("更新京东Cookie失败‼‼", "", "请重试");
+    if (pt_pin_match && pt_key_match) {
+        console.log("pt_pin: " + pt_pin_match[1]);
+        console.log("pt_key: " + pt_key_match[1]);
+        $notify("京东Cookie更新成功🎉", "", "pt_pin: " + pt_pin_match[1] + "\npt_key: " + pt_key_match[1]);
+    } else {
+        console.log("未找到pt_pin或pt_key。");
+        $notify("京东Cookie更新失败‼️", "", "请检查是否已登录并重试");
+    }
 }
+
+// 执行脚本
+if ($request && $request.method != 'OPTIONS') {
+    getCookiesAndLog();
+}
+
 $done({});
